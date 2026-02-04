f74f00dd065455fc9a47d6a5b43c378a609acc91
Kiarash shahbazi <77072333+KiaShahbazi13@users.noreply.github.com>
2026-02-04 18:05:54 +0330

    Version 3.2.0

---
+++ SimpleSql-3.2.0/SimpleSql/__init__.py
@@ -0,0 +1,11 @@
+#  -----------------------
+# |                       |
+# |   By @KiaShahbazi13   |
+# |                       |
+#  -----------------------
+# -Refrence 
+# -- github:
+# ---- https://github.com/KiaShahbazi13/SimpleSql
+__version__ = "3.2.0"
+
+from .SimpleSql import sql
